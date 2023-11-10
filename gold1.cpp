a7cef771cd5df78cbb5600227c3270252ce2a285
Aditya Barnwal <90963060+adiwish@users.noreply.github.com>
2023-11-11 00:02:23 +0530

    done

---
+++ gold1.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include <iostream>
+using namespace std;
+#define adiwish ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+void code(){
+    
+
+}
+
+int32_t main(){
+        code();
+        cout<<endl;   
+}
